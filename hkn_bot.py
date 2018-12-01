a18360331b9a48ac4c7b03d4ec5ec2b7c6847cf9
vina97 <45501717+vina97@users.noreply.github.com>
2018-12-01 11:59:29 +0100

    Update hkn_bot.py
    tutoring part added

--- hkn_bot.py
+++ hkn_bot.py
@@ -9,10 +9,20 @@ dispatcher = updater.dispatcher
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    
+   
+tutor = {'elettrotecnica' : 'tutoring di elettrotecnica il 30/11/2018', 'algoritmi' : 'tutoring di algoritmi e programmazione il 26/11/2018'}
+
+def tutoring(bot, update):
+   for stringa in tutor.keys():
+      bot.send_message(chat_id=update.message.chat_id, text=tutor[stringa])
 
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+tutoring_handler = CommandHandler('tutoring', tutoring)
+dispatcher.add_handler(tutoring_handler)
 
 updater.start_polling()
 
