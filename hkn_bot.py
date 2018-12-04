63f0684f233de888d42ed2ccd4c1ed10d9e4f9f3
Davide Fiorino <davide.fiorino@yahoo.it>
2018-12-04 18:09:03 +0100

    News and Events handler added
    by Davide Fiorino and Angelo Marcianò.

--- hkn_bot.py
+++ hkn_bot.py
@@ -6,6 +6,9 @@ import os
 import telegram
 from telegram.ext import Updater
 import filters
+import time
+import datetime
+
 
 # Uncomment for debug
 #print(os.environ['HKN_BOT_TOKEN'])
@@ -32,7 +35,7 @@ from telegram.ext import MessageHandler
 # Study groups informations  
 tutor = {'elettrotecnica' : 'tutoring di elettrotecnica il 30/11/2018', 'algoritmi' : 'tutoring di algoritmi e programmazione il 26/11/2018'}
 
-# Study groups handler
+#-- Study groups handler
 def tutoring(bot, update):
    for stringa in tutor.keys():
       bot.send_message(chat_id=update.message.chat_id, text=tutor[stringa])
@@ -41,7 +44,7 @@ filter_tutoring = filters.FilterTutoring()
 tutoring_handler = MessageHandler(filter_tutoring, tutoring)
 dispatcher.add_handler(tutoring_handler)
 
-#about handler
+#-- About handler
 def about(bot, update):
    in_file = open("about.txt", "r")
    bot.send_message(chat_id=update.message.chat_id, text=in_file.read())
@@ -51,5 +54,49 @@ about_handler = MessageHandler(filter_about, about)
 dispatcher.add_handler(about_handler)
 
 
-updater.start_polling()
+#-- News handler
+class News:
+  title = 'A title'
+  content = 'Text'
+  date = datetime.date(1943,3,13)  #year, month, day
+  #print(date.strftime("%A"))
+  def __init__(self, title, content, date):
+     self.title = title
+     self.content = content
+     self.date = date
 
+# demo datas
+news1 = News(title='News 1', content='Lorem ipsum dolor sit', date=datetime.date(2018,3,13))
+news2 = News(title='News 2', content='Consectetur adipiscing elit', date=datetime.date(2018,12,25))
+newsList = [news1, news2]
+
+def fetch_news(bot, update):
+   for theNews in newsList:
+      bot.send_message(chat_id=update.message.chat_id, text=theNews.content)
+
+
+#-- Event handler
+class Event:
+  title = 'A title'
+  description = 'Text'
+  date = datetime.date(1943,3, 13)  #year, month, day
+  def __init__(self, title, description, date):
+     self.title = title
+     self.description = description
+     self.date = date
+
+# demo datas
+event1 = Event(title='Event 1', description='Evento di marzo (passato)', date=datetime.date(2018,3,13))
+event2 = Event(title='Event 2', description='Evento di dicembre', date=datetime.date(2018,12,25))
+eventList = [event1, event2]
+
+def fetch_events(bot, update):
+   for theEvent in eventList:
+      todayDate = datetime.datetime.now()
+      if theEvent.date >= todayDate: #do not print past events
+         bot.send_message(chat_id=update.message.chat_id, text=theEvent.description)
+
+
+
+
+updater.start_polling()
