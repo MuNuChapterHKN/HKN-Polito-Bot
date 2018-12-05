e412b8cfff02928814daaa1be199821f3bca7c10
SimoneDutto <simonedutto8@gmail.com>
2018-12-05 18:00:34 +0100

    Add script to parse tutoring page to obtain schedule

---
+++ tutoring parse.py
@@ -0,0 +1,19 @@
+# start of the code
+import re
+import html2text
+import urllib
+
+fp = urllib.request.urlopen("https://hknpolito.org/tutoring/")
+mybytes = fp.read()
+
+mystr = mybytes.decode("utf8")
+fp.close()
+mystr = html2text.html2text(mystr)
+m=mystr.split("* ###")
+m.pop(0)
+for el in m:
+    sub_els=el.split('\n',7)
+    sub_els.pop()
+    print("----------------")
+    for sub_el in sub_els:
+        print(sub_el)
