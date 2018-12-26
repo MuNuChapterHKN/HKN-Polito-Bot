574700281b815cd73ed21e0244daaba1a6c3e33a
Gialbo <gilbertomanunza@gmail.com>
2018-12-26 01:44:44 +0100

    Add admins options and possibility to reply to answers

--- hkn_bot.py
+++ hkn_bot.py
@@ -9,15 +9,15 @@ from telegram.ext import Updater
 # Handling commands
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
-#tutoring part imports
+# Tutoring part imports
 import re
 import html2text
 from urllib.request import urlopen
-#events - news import
+# Events - news import
 import time
 import datetime
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Filters
 from wordpress_xmlrpc import Client
 from wordpress_xmlrpc.methods import posts
 
@@ -40,6 +40,23 @@ def send_action(action):
 # The message "is typing" appears while the bot is processing messages
 send_typing_action = send_action(ChatAction.TYPING)
 
+from functools import wraps
+
+LIST_OF_ADMINS = []
+with open("admins.txt", "r") as admins_file:
+    for line in admins_file:
+        LIST_OF_ADMINS.append(int(line))
+
+def restricted(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in LIST_OF_ADMINS:
+            print("Unauthorized access denied for {}.".format(user_id))
+            return
+        return func(bot, update, *args, **kwargs)
+    return wrapped
+
 # Uncomment for debug
 #print(os.environ['HKN_BOT_TOKEN'])
 
@@ -61,7 +78,7 @@ dispatcher.add_handler(start_handler)
 
 
 import tutor
-#save tutoring groups in file
+# Save tutoring groups in file
 tutor.tutoringFile()
 
 
@@ -69,7 +86,7 @@ filter_tutoring = filters.FilterTutoring()
 tutoring_handler = MessageHandler(filter_tutoring, tutor.tutoring)
 dispatcher.add_handler(tutoring_handler)
 
-#-- About handler
+# About handler
 @send_typing_action
 def about(bot, update):
     in_file = open("about.txt", "r", encoding="utf-8")
@@ -81,7 +98,7 @@ about_handler = MessageHandler(filter_about, about)
 dispatcher.add_handler(about_handler)
 
 
-#-- Questions handler
+# Questions handler
 @send_typing_action
 def questions(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Fai una domanda al MuNu Chapter di Eta Kappa Nu")
@@ -90,13 +107,16 @@ filter_questions = filters.FilterQuestions()
 questions_handler = MessageHandler(filter_questions, questions)
 dispatcher.add_handler(questions_handler)
 
-#-- Answer appender to file
-# Answers must containts with "?"
+# Answer appender to file
+# Answers must contains "?"
 def answers(bot,update):
     out_file = open("questions.txt","a+")
-    out_file.write(str(update.message.from_user.username)+" - "+update.message.text+"\n")
+    user_id = str(update.effective_user.id)
+    out_file.write(str(update.message.from_user.username)+"-"+user_id+"-"+update.message.text+"\n")
     out_file.close()
     bot.send_message(chat_id=update.message.chat_id, text="La tua domanda è stata registrata, ti risponderemo a breve")
+    for admin in LIST_OF_ADMINS:
+        bot.send_message(chat_id=admin, text="Nuova domanda da: "+str(update.message.from_user.username)+"\n-"+update.message.text+"\n")      
     
 
 filter_answers = filters.FilterAnswers()
@@ -104,8 +124,9 @@ answers_handler = MessageHandler(filter_answers, answers)
 dispatcher.add_handler(answers_handler)
 
 
-#-- News handler
+# News handler
 # Unused in latest commit: Evaluate deletion
+# TODO: REMOVE
 class News:
     title = 'A title'
     content = 'Text'
@@ -116,7 +137,8 @@ class News:
         self.content = content
         self.date = date
 
-# demo datas for news
+# Demo datas for news 
+# TODO: REMOVE
 news1 = News(title='News 1', content='Lorem ipsum dolor sit', date=datetime.date(2018,3,13))
 news2 = News(title='News 2', content='Consectetur adipiscing elit', date=datetime.date(2018,12,25))
 newsList = [news1, news2]
@@ -133,7 +155,7 @@ def fetch_news(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text=content)
 
 
-#-- Event handler
+# Event handler
 class Event:
     title = 'A title'
     description = 'Text'
@@ -146,7 +168,7 @@ class Event:
         self.description = description
         self.date = date
 
-# demo datas for events
+# Demo datas for events
 event1 = Event(title='Event 1', description='Evento di marzo (passato)', date=datetime.datetime(2018,3,13))
 event2 = Event(title='Event 2', description='Evento di dicembre', date=datetime.datetime(2018,12,25))
 event2.imageLink = 'https://hknpolito.org/wp-content/uploads/2018/05/33227993_2066439693603577_8978955090240995328_o.jpg'
@@ -174,6 +196,65 @@ def fetch_events(bot, update):
                 else: continue #skip the sending of the links
                 reply_markup = InlineKeyboardMarkup(keyboard)
                 bot.send_photo(chat_id=update.message.chat_id, caption=theEvent.description, photo=theEvent.imageLink, reply_markup=reply_markup)
+
+# Restricted commands
+
+# Reply to answers handler
+from telegram.ext import ConversationHandler
+
+# Setting up conversation handler to wait for user message
+ANSWER = 1
+def popquestion():
+    question_file = open("questions.txt", "r+")
+    questions = question_file.readlines()
+    if questions == None:
+        question_file.close()
+        return None
+    question_file.seek(0)
+    for q in questions[1:]:
+        question_file.write(q)
+    question_file.truncate()
+    question_file.close()
+    return questions[0].split("-")
+
+@restricted
+def answer(bot, update):
+    question = popquestion()
+    if question == None:
+        bot.send_message(chat_id=update.message.chat_id, text="Non ci sono più domande a cui rispondere")
+        return ConversationHandler.END
+    message = update.message.text 
+    bot.send_message(chat_id=question[1], text="Ciao {} ecco la risposta alla tua domanda:\n{}".format(question[0],message))
+    question_file.close()
+    return ConversationHandler.END
+
+@restricted
+def skip(bot,update):
+    bot.send_message(chat_id=update.message.chat_id, text="Domanda non risposta")
+    return ConversationHandler.END
+
+@restricted
+def cancel(bot, update):    
+    bot.send_message(chat_id=update.message.chat_id, text="Conversazione cancellata")
+    return ConversationHandler.END
+
+@restricted
+def reply(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Rispondi alla domanda: \n")
+    question_file = open("questions.txt","r")
+    bot.send_message(chat_id=update.message.chat_id, text=question_file.readline())
+    question_file.close()
+    return ANSWER
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler("reply", reply)],
+    states={ANSWER: [MessageHandler(Filters.text, answer),
+                    CommandHandler("skip", skip)]
+           },
+    fallbacks=[CommandHandler("cancel", cancel)]
+)
+
+dispatcher.add_handler(conv_handler)
 
 
 
