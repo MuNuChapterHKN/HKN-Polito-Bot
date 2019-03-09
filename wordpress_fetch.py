10d0b47509efe8f924b50b3ecc55cbf5dc387725
Gialbo <gilbertomanunza@gmail.com>
2019-03-09 20:12:11 +0100

    Preparation for hosting

--- wordpress_fetch.py
+++
@@ -1,8 +0,0 @@
-from wordpress_xmlrpc import Client
-from wordpress_xmlrpc.methods import posts
-import os 
-
-def retrieve_posts():
-    client = Client(url = 'https://hknpolito.org/xmlrpc', username = "HKNP0lit0", password = os.environ['HKN_WEB_PASSWORD'])
-    postsdict = client.call(posts.GetPosts())
-    return postsdict
