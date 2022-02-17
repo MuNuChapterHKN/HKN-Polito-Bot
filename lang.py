18889e56b8f8642532cd3b731526f213b99c06d9
gabbiurlaro <s258946@studenti.polito.it>
2022-02-17 20:46:37 +0100

    Start refactoring process

--- lang.py
+++ lang.py
@@ -1,5 +1,8 @@
+"""
+Language class. It retrieves from a json file two json object, organized 
+as key-value pair
+"""
 import json
-# Language dictionaries
 
 with open("it.json", "r", encoding="utf-8") as f:
     lang_it = json.load(f)
