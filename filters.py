33b850e5384627f8ad25f76d7d9e50985dec999e
Gabriele Iurlaro s294917 <s294917@studenti.polito.it>
2022-05-03 09:08:20 +0200

    Modified filter class according to python-telegram-bot v13

--- filters.py
+++ filters.py
@@ -1,74 +1,74 @@
 # Custom filters for message handling
 
-from telegram.ext import BaseFilter
+from telegram.ext import MessageFilter
 from lang import lang_en, lang_it
 
 
-class FilterTutoring(BaseFilter):
+class FilterTutoring(MessageFilter):
     def filter(self, message):
         return lang_en["studygroups"].lower() == message.text.lower() or lang_it[
             "studygroups"].lower() == message.text.lower()
 
 
-class FilterAbout(BaseFilter):
+class FilterAbout(MessageFilter):
     def filter(self, message):
         return lang_en["about"].lower() == message.text.lower() or lang_it["about"].lower() == message.text.lower()
 
 
-class FilterNews(BaseFilter):
+class FilterNews(MessageFilter):
     def filter(self, message):
         return lang_en["news"].lower() == message.text.lower() or lang_it["news"].lower() == message.text.lower()
 
 
-class FilterEvents(BaseFilter):
+class FilterEvents(MessageFilter):
     def filter(self, message):
         return lang_en["events"].lower() == message.text.lower() or lang_it["events"].lower() == message.text.lower()
 
 
-class FilterNewsletter(BaseFilter):
+class FilterNewsletter(MessageFilter):
     def filter(self, message):
         return lang_en["newsletter"].lower() == message.text.lower() or lang_it[
             "newsletter"].lower() == message.text.lower()
 
 
-class FilterQuestions(BaseFilter):
+class FilterQuestions(MessageFilter):
     def filter(self, message):
         return lang_en["askus"].lower() == message.text.lower() or lang_it["askus"].lower() == message.text.lower()
 
 
-class FilterDrive(BaseFilter):
+class FilterDrive(MessageFilter):
     def filter(self, message):
         return lang_en["drive"].lower() == message.text.lower() or lang_it["drive"].lower() == message.text.lower()
 
 
-class FilterBack(BaseFilter):
+class FilterBack(MessageFilter):
     def filter(self, message):
         return lang_en["back"].lower() == message.text.lower() or lang_it["back"].lower() == message.text.lower()
 
 
-class FilterContact(BaseFilter):
+class FilterContact(MessageFilter):
     def filter(self, message):
         return lang_en["contact"].lower() == message.text.lower() or lang_it["contact"].lower() == message.text.lower()
 
 
-class FilterMembers(BaseFilter):
+class FilterMembers(MessageFilter):
     def filter(self, message):
         return lang_en["members"].lower() == message.text.lower() or lang_it["members"].lower() == message.text.lower()
 
 
-class Filterelectronicengineeringgroups(BaseFilter):
+class Filterelectronicengineeringgroups(MessageFilter):
     def filter(self, message):
         return lang_en["electronicengineeringgroups"].lower() == message.text.lower() or lang_it[
             "electronicengineeringgroups"].lower() == message.text.lower()
 
 
-class FilterIt(BaseFilter):
+class FilterIt(MessageFilter):
     def filter(self, message):
         return lang_en["lang_ita"].lower() == message.text.lower() or lang_it[
             "lang_ita"].lower() == message.text.lower()
 
 
-class FilterEn(BaseFilter):
+class FilterEn(MessageFilter):
     def filter(self, message):
         return lang_en["lang_eng"].lower() == message.text.lower() or lang_it[
             "lang_eng"].lower() == message.text.lower()
